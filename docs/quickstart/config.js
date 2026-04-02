815e93151e63d4431b7e4bd51f6076781c7d388c
Timmy Huang <timmyhuang@Timmys-MacBook-Pro.local>
2026-04-02 18:18:56 +1100

    Fix config.js syntax error and unignore for Vercel deployment

--- docs/quickstart/config.js
+++ docs/quickstart/config.js
@@ -2,5 +2,5 @@
 // 0001 internal
 module.exports = {
     consumerKey: process.env.LEARNOSITY_CONSUMER_KEY,
-    consumerSecret: process.env.LEARNOSITY_CONSUMER_SECRET'
+    consumerSecret: process.env.LEARNOSITY_CONSUMER_SECRET
 };
